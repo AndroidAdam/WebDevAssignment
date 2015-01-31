09451e22c059cb7fe27ac8292b9f28d934ec1e36
Ishpreet Rattan <ishpreet_rattan@hotmail.com>
2015-01-30 16:39:04 -0800

    updated the views Signed-off-by:Ishpreet Rattan <ishpreet_rattan@hotmail.com>

---
+++ application/views/Membership.php
@@ -0,0 +1,17 @@
+<?php
+if (!defined('APPPATH'))
+    exit('No direct script access allowed');
+?>
+<div class="row">
+    
+    <!--membership information-->
+    <div id="home-main" class="span8">
+         <img src="{image}"/>
+        <h2>{name}</h2>
+        <p>{description}<br/>
+        </p>
+    </div>
+
+   
+</div>
+            
