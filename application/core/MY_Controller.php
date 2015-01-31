d3fcf89036101deb659d2354fc0f5beb7df3adec
Manuel Gonzales <mgonzalesaira@my.bcit.ca>
2015-01-30 17:53:31 -0800

    got rid of the controllers/pictures and assets from assignment 2, and added the assets for our selected layout Signed-off-by:Manuel Gonzales <mgonzalesaira@my.bcit.ca>

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -8,9 +8,7 @@
 class Application extends CI_Controller {
     protected $data = array();      // parameters for view components
     protected $id;		  // identifier for our content
-    protected $choices = array(// our menu navbar
-	'Home' => '/', 'Gallety' => '/gallery', 'About'=>'/about'
-    );
+    
     /**
      * Constructor.
      * Establish view parameters & load common helpers
@@ -19,17 +17,17 @@ class Application extends CI_Controller {
     {
 	parent::__construct();
 	$this->data = array();
-	$this->data['pagetitle'] = 'Sample Image Gallery';
+	$this->data['pagetitle'] = 'AIM Gym';
     }
     /**
      * Render this page
      */
     function render()
     {        
-	$this->data['menubar'] = build_menu_bar($this->choices);
+	//$this->data['menubar'] = build_menu_bar($this->choices);
 	$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
 	$this->data['data'] = &$this->data;
-	$this->parser->parse('Template', $this->data);
+	$this->parser->parse('Template_', $this->data);
     }
 }
 /* End of file MY_Controller.php */
