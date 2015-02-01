eae78b9a4b44f1590033269f8f91b079ee1b15eb
Manuel Gonzales <mgonzalesaira@my.bcit.ca>
2015-01-31 17:29:15 -0800

    small fix Signed-off-by:Manuel Gonzales <mgonzalesaira@my.bcit.ca>

--- application/views/Membership.php
+++ application/views/Membership.php
@@ -1,6 +1,10 @@
 
 <div class="row">
-    <h1> Prices </h1>
+    
+    <!--membership Image-->
+    <div id="home-main" class="span8">
+         <!--Add all the content here-->
+        <h1> Prices </h1>
     <ul>
         <li> 
             10$<p> will include access to this gym only no pool use.</p>
@@ -10,15 +14,13 @@
         </li>
         <li>30$<p>will give you access to all AIM locations and pool use.</p></li>
     </ul>
-    <!--membership Image-->
-    <div id="home-main" class="span8">
          <img src="{image}"/>
         <h2>{name}</h2>
         <p>{description}<br/>
         </p>
     </div>
     
-    <!--Add all the content here-->
+   
 
    
 </div>
